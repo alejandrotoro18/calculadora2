8a5a9c70f79406befec3b05d39613a9d95d96aa6
alejandrotoro18 <43111688+alejandrotoro18@users.noreply.github.com>
2022-11-27 08:43:54 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+titulo="Calculadora"
+
+opernadoA:number=0;
+opernadoB:number=0;
+resultado:number=0;
+
+sumar():void {
+  this.resultado = this.opernadoA+this.opernadoB;
+}
+
 }
